7f0df5c2640e035326cbc54f929752a75831166d
Aditya Kushwaha <50064160+kaditya97@users.noreply.github.com>
2021-07-07 12:36:40 +0545

    Update index.js

--- sawp-frontend/src/index.js
+++ sawp-frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/style.scss';
 import axios from 'axios';
     
